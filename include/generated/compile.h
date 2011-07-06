2cd1ac8baf5cd3256aaa84dbc74b0f93bf8d637f
Savides Edward <edward.savides@gmail.com>
2011-07-06 01:13:50 -0400

    Initial commit. 8.4.4.5 with 2.6.36.4 patch, and some mildly misbehaving dvfs/cpuclock stuff (as in, 1.64 will occasionally throw a hissy fit). Also have the touchpad-tap sysfs enable/disable patch (netarchy). A few changes to some files that like to be 'chatty-cattys' all over the kernel log - shutting up the messages on the 'obvious' end of the spectrum. Don't need devices saying "hey, I'm ok!" every 5 seconds to tell you everythings peachy. I have the usual adjustments to the thermal throttling constants as well. CPU/GPU/AVP/VDE/MPE and so on has been upped.. The general rule I follow is that if I know what it does, I'll attempt to make it do xyz faster.. Even in the pointless case of MPE (media processing engine) - which is, apparently not used at all. btw.. Seems someone implemented 2D accel.. as in, its not software anymore.. As evidenced by higher quadrant 2d scores and just overall 'smoothness'.. Quadrant scores are nearly halved - boohoo.. most likely from (huge) drop in I/O score - But overall, everything 'feels' faster with 3.1 and crashes less..

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 12 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#12 SMP PREEMPT Tue Jul 5 20:22:43 EDT 2011"
+#define LINUX_COMPILE_TIME "20:22:43"
+#define LINUX_COMPILE_BY "edward"
+#define LINUX_COMPILE_HOST "LinuxRaidX64"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.5.2 (Ubuntu/Linaro 4.5.2-8ubuntu3) "
